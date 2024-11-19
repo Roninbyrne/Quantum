be44210f58bac72cfe65be8f03092e0920b26d4f
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-11-19 22:00:40 +0530

    TEST

--- Quantum/plugins/bot/privacy.py
+++ Quantum/plugins/bot/privacy.py
@@ -7,7 +7,7 @@ import config
 TEXT = f"""
 🔒 **Privacy Policy for {app.mention} !**
 
-Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: [Privacy Policy]({config.PRIVACY_LINK}).
+Your privacy is important to us. To learn more about how we collect, use, and protect your data, please review our Privacy Policy here: **Privacy Policy**.
 
 If you have any questions or concerns, feel free to reach out to our [support team]({config.SUPPORT_GROUP}).
 """
