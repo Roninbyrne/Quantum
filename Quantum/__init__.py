482c9e9fa89826bec79b222880323e7f198e4487
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2024-11-19 23:34:13 +0530

    s

--- Quantum/__init__.py
+++ Quantum/__init__.py
@@ -5,6 +5,18 @@ from Quantum.core.userbot import Userbot
 from Quantum.misc import dbb, heroku
 
 from .logging import LOGGER
+from inspect import getfullargspec
+from pyrogram.types import Message
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
 
 dirr()
 dbb()
